a41d16420423aca4b30552bc74f56109081cb8bf
sosmagiceu <v.vynohradova@gmail.com>
2025-12-16 12:50:18 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
-
-document.querySelectorAll('[data-carousel]').forEach(c=>{
-  let i=0, track=c.querySelector('.tm-carousel__track');
-  setInterval(()=>{i=(i+1)%5;track.style.transform=`translateX(${-i*100}%)`},3000);
+import { wireMenuActive } from "./menu.js";
+import { initCarousels } from "./carousel.js";
+document.addEventListener("DOMContentLoaded", ()=>{
+  wireMenuActive();
+  initCarousels();
 });
