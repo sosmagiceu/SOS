82f8e2ab07df8f9d9db63f30d3fff08c5055c2eb
sosmagiceu <v.vynohradova@gmail.com>
2025-12-19 11:48:39 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 import { wireMenuActive } from "./menu.js";
 import { initCarousels } from "./carousel.js";
+import { initTopbarMenu } from "./topbar-menu.js";
 document.addEventListener("DOMContentLoaded", ()=>{
   wireMenuActive();
   initCarousels();
+  initTopbarMenu();
 });
