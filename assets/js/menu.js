05fb4f8c385680d803d0783640b2c63fbb6492b2
sosmagiceu <v.vynohradova@gmail.com>
2025-12-19 15:33:47 -0500

    Delete assets/js/menu.js

--- assets/js/menu.js
+++
@@ -1,5 +0,0 @@
-import { qsAll } from "./utils.js";
-export function wireMenuActive(){
-  const page=document.body.dataset.page;
-  qsAll("[data-nav]").forEach(a=>a.classList.toggle("is-active", a.dataset.nav===page));
-}
