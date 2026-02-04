9bd59740003bbb660ab10e2dc94b513dc5b1dc55
sosmagiceu <v.vynohradova@gmail.com>
2026-02-04 13:01:02 -0400

    Add files via upload

--- assets/js/vendor/tooplate-2144-parallax-scripts.js
+++ assets/js/vendor/tooplate-2144-parallax-scripts.js
@@ -32,29 +32,8 @@ window.addEventListener('scroll', () => {
    }
 });
 
-// Mouse follower
-const mouseFollower = document.getElementById('mouseFollower');
-let mouseX = 0,
-   mouseY = 0;
-let followerX = 0,
-   followerY = 0;
 
-document.addEventListener('mousemove', (e) => {
-   mouseX = e.clientX;
-   mouseY = e.clientY;
-});
-
-// Smooth animation for mouse follower
-function animateFollower() {
-   followerX += (mouseX - followerX) * 0.1;
-   followerY += (mouseY - followerY) * 0.1;
-
-   mouseFollower.style.left = followerX + 'px';
-   mouseFollower.style.top = followerY + 'px';
-
-   requestAnimationFrame(animateFollower);
-}
-animateFollower();
+// Mouse follower removed (not used on SOS Magic)
 
 // Interactive hover effects for rectangles
 const rectangles = document.querySelectorAll('.rect');
